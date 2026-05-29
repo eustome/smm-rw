655a7577481bbb62cea9e6cdfe0571989b9fae85
eustome <ggq3242@gmail.com>
2026-05-29 17:49:00 +0300

    Refactor build.bat to use vswhere for Visual Studio path

--- smm/build.bat
+++ smm/build.bat
@@ -1,26 +1,49 @@
 @echo off
-set MSVC=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207
-set SRC=c:\Users\n\Desktop\spoofer\smm
-set EDK=%SRC%\edk2
+setlocal
 
-set CL_FLAGS=/nologo /c /GS- /Gy /W3 /O1 /Oi- /I"%EDK%\MdePkg\Include\X64" /I"%EDK%\MdePkg\Include" /I"%EDK%\ShellPkg\Include" /I"%EDK%\CryptoPkg\Include" /I"%SRC%" /D_AMD64_
+set SRC=%~dp0
+if "%SRC:~-1%"=="\" set SRC=%SRC:~0,-1%
+set EDK=%SRC%\..\edk2
 
-echo [*] main.c
-"%MSVC%\bin\Hostx64\x64\cl.exe" %CL_FLAGS% /Fo"%SRC%\main.obj" "%SRC%\main.c"
+set VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+if not exist "%VSWHERE%" (
+    echo vswhere.exe not found. Install Visual Studio 2022 with C++ tools.
+    exit /b 1
+)
+for /f "usebackq delims=" %%i in (
+    `"%VSWHERE%" -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`
+) do set VS_PATH=%%i
+if not defined VS_PATH (
+    echo Visual Studio with C++ workload not found.
+    exit /b 1
+)
+call "%VS_PATH%\VC\Auxiliary\Build\vcvarsall.bat" x64 >nul 2>&1
+
+set CL_FLAGS=/nologo /c /GS- /Gy /W3 /O1 /Oi- ^
+    /I"%EDK%\MdePkg\Include\X64" ^
+    /I"%EDK%\MdePkg\Include" ^
+    /I"%EDK%\ShellPkg\Include" ^
+    /I"%SRC%" ^
+    /D_AMD64_
+
+echo main.c
+cl.exe %CL_FLAGS% /Fo"%SRC%\main.obj" "%SRC%\main.c"
 if errorlevel 1 goto :fail
 
-echo [*] util.c
-"%MSVC%\bin\Hostx64\x64\cl.exe" %CL_FLAGS% /Fo"%SRC%\util.obj" "%SRC%\util.c"
+echo util.c
+cl.exe %CL_FLAGS% /Fo"%SRC%\util.obj" "%SRC%\util.c"
 if errorlevel 1 goto :fail
 
-echo [*] linking...
-"%MSVC%\bin\Hostx64\x64\link.exe" /nologo /DLL /NODEFAULTLIB /ENTRY:EfiMain /SUBSYSTEM:EFI_BOOT_SERVICE_DRIVER /OUT:"%SRC%\smm.efi" "%SRC%\main.obj" "%SRC%\util.obj"
+echo linking...
+link.exe /nologo /DLL /NODEFAULTLIB /ENTRY:EfiMain ^
+    /SUBSYSTEM:EFI_BOOT_SERVICE_DRIVER ^
+    /OUT:"%SRC%\smm.efi" "%SRC%\main.obj" "%SRC%\util.obj"
 if errorlevel 1 goto :fail
 
-echo [+] smm.efi
+echo smm.efi
 del "%SRC%\*.obj" 2>nul
-goto :end
+exit /b 0
 
 :fail
-echo [-] FAIL
-:end
+echo FAIL
+exit /b 1
